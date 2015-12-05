f81afecec62c819aab5fe63a2e169ec77fdb776c
omnivoorhorst <jaspervoorhorst@gmail.com>
2015-12-05 21:01:40 +0100

    Vuurspuwer

---
+++ 5/scripts/script.js
@@ -0,0 +1,8 @@
+
+function toggleContent() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
