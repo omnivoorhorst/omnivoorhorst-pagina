81e28a53ad9ea5e71d5ee93181ace7dc6c90d514
omnivoorhorst <jaspervoorhorst@gmail.com>
2015-12-06 13:36:38 +0100

    Spatiespel

---
+++ 6/scripts/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+
+
+   $("#s1").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s2").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s3").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s4").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s5").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s6").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s7").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s8").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s9").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+   $("#s10").one( "click", function(){
+        $(this).css("background-color", "#ff5512");
+        $('#scorebord').html(function(i, val) { return val*1+1 });
+    });
+
+
+});
